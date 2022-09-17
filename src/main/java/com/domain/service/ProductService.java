c9abadecb8517e709484e5278c395268a8bdcd95
Faris sahaluddin islam <faris.salahuddin@iconpln.co.id>
2022-09-17 17:36:57 +0700

    add delete and update

--- src/main/java/com/domain/service/ProductService.java
+++ src/main/java/com/domain/service/ProductService.java
@@ -1,22 +1,24 @@
 package com.domain.service;
 
 import com.domain.entity.Product;
+import com.domain.utils.RandomNumber;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private static List<Product> products = new ArrayList<Product>(){
         {
-            add (new Product(System.currentTimeMillis(), "001", "Product 0001", 1000));
-            add (new Product(System.currentTimeMillis(), "002", "Product 0002", 2000));
-            add (new Product(System.currentTimeMillis(), "003", "Product 0003", 3000));
-            add (new Product(System.currentTimeMillis(), "004", "Product 0004", 4000));
-            add (new Product(System.currentTimeMillis(), "005", "Product 0005", 5000));
+            add (new Product(RandomNumber.getRandom(1000, 9999), "001", "Product 0001", 1000));
+            add (new Product(RandomNumber.getRandom(1000, 9999), "002", "Product 0002", 2000));
+            add (new Product(RandomNumber.getRandom(1000, 9999), "003", "Product 0003", 3000));
+            add (new Product(RandomNumber.getRandom(1000, 9999), "004", "Product 0004", 4000));
+            add (new Product(RandomNumber.getRandom(1000, 9999), "005", "Product 0005", 5000));
         }
     };
 
@@ -25,7 +27,20 @@ public class ProductService {
     }
 
     public void addProduct(Product product){
-        product.setId(System.currentTimeMillis());
+        product.setId(RandomNumber.getRandom(1000, 9999));
+        products.add(product);
+    }
+
+    public void deleteById(long id){
+        products.removeIf(p -> p.getId()==id);
+    }
+
+    public Optional<Product> findById(long id){
+        return products.stream().filter(p -> p.getId() ==id).findFirst();
+    }
+
+    public void update(Product product){
+        deleteById(product.getId());
         products.add(product);
     }
 
---
+++ src/main/java/com/domain/utils/RandomNumber.java
@@ -0,0 +1,9 @@
+package com.domain.utils;
+
+public class RandomNumber {
+
+    public static long getRandom(long min, long max){
+    long number = min + (long) (Math.random() * (max-min));
+        return number;
+    }
+}
